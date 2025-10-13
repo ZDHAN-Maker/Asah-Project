f5aa7b93812388f57778efab70af6e144676e558
Dan <uprise419@gmail.com>
2025-10-13 21:20:01 +0700

    membuat users dan paylist folder

--- notes-app-back-end/src/app/api/albums/routes.js
+++ notes-app-back-end/src/app/api/albums/routes.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const authMiddleware = require('../../middleware/authmiddleware');
 
 function createAlbumsRouter(handler) {
   const router = express.Router();
 
-  router.post('/', handler.postAlbumHandler);
+  router.post('/', authMiddleware, handler.postAlbumHandler);
 
-  router.get('/:id', handler.getAlbumByIdHandler);
+  router.get('/:id', authMiddleware, handler.getAlbumByIdHandler);
 
-  router.put('/:id', handler.putAlbumByIdHandler);
+  router.put('/:id', authMiddleware, handler.putAlbumByIdHandler);
 
-  router.delete('/:id', handler.deleteAlbumByIdHandler);
+  router.delete('/:id', authMiddleware, handler.deleteAlbumByIdHandler);
 
   return router;
 }
--- notes-app-back-end/src/app/api/songs/routes.js
+++ notes-app-back-end/src/app/api/songs/routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const authMiddleware = require('../../middleware/authmiddleware');
 
 const createSongsRouter = (handler) => {
   const router = express.Router();
 
-  router.post('/', (req, res) => handler.postSongHandler(req, res));
-  router.get('/', (req, res) => handler.getSongsHandler(req, res));
-  router.get('/:id', (req, res) => handler.getSongByIdHandler(req, res));
-  router.put('/:id', (req, res) => handler.putSongByIdHandler(req, res));
-  router.delete('/:id', (req, res) => handler.deleteSongByIdHandler(req, res));
+  router.post('/', authMiddleware, (req, res) => handler.postSongHandler(req, res));
+  router.get('/', authMiddleware, (req, res) => handler.getSongsHandler(req, res));
+  router.get('/:id', authMiddleware, (req, res) => handler.getSongByIdHandler(req, res));
+  router.put('/:id', authMiddleware, (req, res) => handler.putSongByIdHandler(req, res));
+  router.delete('/:id', authMiddleware, (req, res) => handler.deleteSongByIdHandler(req, res));
 
   return router;
 };
