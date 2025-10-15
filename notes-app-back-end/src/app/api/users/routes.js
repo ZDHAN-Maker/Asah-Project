93781862acda2b0f37d3242cda09dc457931c36f
Dan <uprise419@gmail.com>
2025-10-15 10:13:49 +0700

    memperbaiki route users

--- notes-app-back-end/src/app/api/users/routes.js
+++ notes-app-back-end/src/app/api/users/routes.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const UsersHandler = require('./handler');
 
-const createUsersRouter = () => {
+const createUsersRouter = (usersHandler) => {
   const router = express.Router();
-  const usersHandler = new UsersHandler();
 
-  router.post('/', (req, res) => usersHandler.postUserHandler(req, res));
-  router.post('/authentications', (req, res) => usersHandler.loginHandler(req, res));
-  router.put('/authentications', (req, res) => usersHandler.refreshHandler(req, res));
-  router.delete('/authentications', (req, res) => usersHandler.logoutHandler(req, res));
+  router.post('/', usersHandler.postUserHandler.bind(usersHandler));
+  router.post('/authentications', usersHandler.loginHandler.bind(usersHandler));
+  router.put('/authentications', usersHandler.refreshHandler.bind(usersHandler));
+  router.delete('/authentications', usersHandler.logoutHandler.bind(usersHandler));
 
   return router;
 };
