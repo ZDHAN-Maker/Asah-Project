e16c975d39a586ecfb7c7eee5949c0e6b54f0cd1
Dan <uprise419@gmail.com>
2025-10-17 14:16:32 +0700

    membuat validator

--- notes-app-back-end/src/app/api/collaborations/validator.js
+++ notes-app-back-end/src/app/api/collaborations/validator.js
@@ -2,6 +2,15 @@ const Joi = require('joi');
 const InvariantError = require('../../utils/error/InvariantError');
 
 const CollaboratorSchema = Joi.object({
-    playlistId: Joi.string().required(),
-    userId: Joi.string().required()
+  playlistId: Joi.string().required(),
+  userId: Joi.string().required(),
 });
+
+function validatorCollaborator(payload) {
+  const { error } = CollaboratorSchema.validate(payload, { abortearly: false });
+  if (error) {
+    throw new InvariantError(error.message);
+  }
+}
+
+module.exports = validatorCollaborator;
